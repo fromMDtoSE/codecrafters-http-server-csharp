78b36f0ffd57e19924de6e488498e19bdf74ba73
RaedMazahreh <raed@simplify9.com>
2024-04-16 09:51:28 +0300

    pass 4th stage

--- src/Server.cs
+++ src/Server.cs
@@ -20,10 +20,16 @@ try
         byte[] buffer = new byte[1024];
         await stream.ReadAsync(buffer, 0, buffer.Length);
         string request = Encoding.UTF8.GetString(buffer);
-        Console.WriteLine(request);
-        bool basePathOrEcho = request.Split("\r\n")[0].Split(" ")[1] == "/" || request.Split("\r\n")[0].Split(" ")[1].StartsWith("/echo");
-        string randomStringFromRequest = request.Split("\r\n")[0].Split(" ")[1].Split("/")[1];
+
+        string requestPath = request.Split(" ")[1];
+        Console.WriteLine(requestPath);
+
+        bool basePathOrEcho = requestPath == "/" || requestPath == "/echo";
+        int reqestPathLength = requestPath.Length;
+
+        string randomStringFromRequest = requestPath == "/echo" ? request.Split(" ")[2].Substring(6, reqestPathLength) : string.Empty;
         Console.WriteLine(randomStringFromRequest);
+
         byte[] response = Encoding.UTF8.GetBytes(basePathOrEcho ? $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {randomStringFromRequest.Length}\r\n\r\n{randomStringFromRequest}"
         : "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found");
         await stream.WriteAsync(response);
