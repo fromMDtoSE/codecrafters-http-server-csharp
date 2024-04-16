3f9beb896324b975e81250a0fec58532c6b1ce9d
RaedMazahreh <raed@simplify9.com>
2024-04-16 10:11:13 +0300

    pass 5th stage

--- src/Server.cs
+++ src/Server.cs
@@ -16,29 +16,21 @@ try
         using var tcpClientHandler = await server.AcceptTcpClientAsync();
         await using NetworkStream stream = tcpClientHandler.GetStream();
 
-        // read the stream and echo back with the random string given
         byte[] buffer = new byte[1024];
         await stream.ReadAsync(buffer, 0, buffer.Length);
         string request = Encoding.UTF8.GetString(buffer);
-        Console.WriteLine(request);
-
-        // string requestPath = request.Split(" ")[1];
-        // bool basePathOrEcho = requestPath == "/" || requestPath.Contains("/echo");
-        // int reqestPathLength = requestPath.Length;
-
-        // string randomStringFromRequest = requestPath.Contains("/echo") ? requestPath.Split("/echo/")[1] : string.Empty;
-        // Console.WriteLine(randomStringFromRequest);
-
-        // byte[] response = Encoding.UTF8.GetBytes(basePathOrEcho ? $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {randomStringFromRequest.Length}\r\n\r\n{randomStringFromRequest}"
-        // : "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found");
 
         string requestPath = request.Split(" ")[1];
-        bool basePathOrAgent = requestPath == "/" || requestPath.Contains("/user-agent");
+        bool okStatusCode = requestPath == "/" || requestPath.Contains("/echo") || requestPath.Contains("/user-agent");
+        int reqestPathLength = requestPath.Length;
 
-        string userAgent = request.Split("User-Agent: ")[1].Split("\r\n")[0];
-        Console.WriteLine(userAgent);
+        string responseContent = requestPath.Contains("/echo")
+        ? requestPath.Split("/echo/")[1] : requestPath.Contains("/user-agent")
+        ? request.Split("User-Agent: ")[1].Split("\r\n")[0] : string.Empty;
 
-        byte[] response = Encoding.UTF8.GetBytes(basePathOrAgent ? $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgent.Length}\r\n\r\n{userAgent}"
+        Console.WriteLine(responseContent);
+
+        byte[] response = Encoding.UTF8.GetBytes(okStatusCode ? $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {responseContent.Length}\r\n\r\n{responseContent}"
         : "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found");
         await stream.WriteAsync(response);
     }
